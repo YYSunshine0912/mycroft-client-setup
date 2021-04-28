5efd955c57a14b51380fd418b5e548d588311388
YYSunshine0912 <77474707+YYSunshine0912@users.noreply.github.com>
2021-04-28 18:12:38 +0800

    Update add_account_internal.py

--- add_account_internal.py
+++ add_account_internal.py
@@ -22,6 +22,5 @@ print(data)
 response = requests.post('http://42.60.37.128:5002/api/account',data=json.dumps(new_account_request),headers = {'Content-Type':'application/json'})
 
 print(response.content)
-#Add open_dataset agreement for STT google cloud API
-#insert into account.account_agreement(account_id, agreement_id, accept_date) values('d214b11c-cf7a-4c5f-8b53-f8cc3b4e31fb','0b83ae2d-9b5e-4993-ae55-d9036ee79af6',CURRENT_DATE);
+
   
